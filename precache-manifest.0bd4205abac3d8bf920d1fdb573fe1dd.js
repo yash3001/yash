5ed55ea833c67f7e2b2fbc1ebc69925f77e20871
yash3001 <yashchauhan197@gmail.com>
2020-10-23 15:39:01 +0530

    Updates

---
+++ precache-manifest.0bd4205abac3d8bf920d1fdb573fe1dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b53aee6ecfb6e66d92ccb326acff24f",
+    "url": "/yash/index.html"
+  },
+  {
+    "revision": "378f4db590429572082a",
+    "url": "/yash/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7816bfab7f5de4a78e72",
+    "url": "/yash/static/js/2.96e320ec.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/yash/static/js/2.96e320ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378f4db590429572082a",
+    "url": "/yash/static/js/main.c2e7b18a.chunk.js"
+  },
+  {
+    "revision": "a27bf5873fde5ae4985a",
+    "url": "/yash/static/js/runtime-main.7d20077f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yash/static/media/logo.5d5d9eef.svg"
+  }
+]);
